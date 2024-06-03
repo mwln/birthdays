46b1243c0b6cfcccd05f4d0505794d3555f16da0
Marty Whelan <martywhelan73@gmail.com>
2024-06-03 17:08:38 -0230

    setup db functions

--- src/db/index.ts
+++ src/db/index.ts
@@ -13,3 +13,31 @@ export async function migrate() {
     );
 }
 
+export async function setBirthday(user: string, day: string): Promise<boolean> {
+    const result = await db.execute({
+        sql: "INSERT OR REPLACE INTO users (id, day) VALUES (?, ?)",
+        args: [user, day]
+    });
+    return result.rowsAffected === 1;
+}
+
+export async function deleteUser(user: string): Promise<boolean> {
+    const result = await db.execute({
+        sql: "DELETE FROM users WHERE id = ?",
+        args: [user]
+    });
+    return result.rowsAffected > 0;
+}
+
+export async function getBirthday(user: string): Promise<string> {
+    const result = await db.execute({
+        sql: "SELECT day FROM users WHERE id = ?",
+        args: [user]
+    });
+    console.log(result.rows)
+    return result.rows[0][0] as string
+}
+
+// TODO: finish this function implementation
+export async function getUpcomingBirthdays() { }
+
